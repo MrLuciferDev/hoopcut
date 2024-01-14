d2ffc6b1d28fb72fb7730957fdde9d63bbf44204
sahil-me <sahil.activedev@gmail.com>
2024-01-14 16:00:18 +0530

    transition

--- script.js
+++ script.js
@@ -92,3 +92,68 @@ $(document).ready(function () {
 
 
 
+  function delay(n)
+{
+    n=n || 2000;
+    return new Promise((done)=>{
+        setTimeout(()=>{
+    },n);
+    });
+}
+
+
+  function pageTransition()
+  {
+    let tl=gsap.timeline();
+    tl.to(".loading-screen",{
+      duration:1.2,
+      width:"100%",
+      left:"0%",
+      ease:"Expo.easeInOut"
+    });
+
+    tl.to(".loading-screen",{
+      duration:1,
+      width:"100%",
+      left:"100%",
+      ease:"Expo.easeInOut",
+      delay:0.3
+    });
+    tl.set(".loading-screen",{left:"-100%"});
+  }
+
+  function contentAnimation(){
+    let tl = gsap.timeline();
+    tl.to(".hello h1",{
+      top:0,
+      duration:1,
+      ease:"power3.inOut",
+      delay:0.75,
+    });
+  }
+
+
+
+    barba.init({
+      sync:true,
+      transitions:[
+        {
+          async leave(data){
+            const done=this.async();
+
+            pageTransition();
+            await delay(1);
+            done();
+          },
+          async enter(data) {
+            contentAnimation();
+          },
+          async once(data)
+          {
+            contentAnimation();
+          },
+        },
+      ],
+    });
+  
+
